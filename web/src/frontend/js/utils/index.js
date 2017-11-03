0367fcbfec7d2ab526af93882adeac15e01d520c
Andreas Lund <andreas.lund@nav.no>
2017-11-03 07:41:36 +0100

    Endrer URL mot eventdistributøren til å gå igjennom ny VS

---
+++ web/src/frontend/js/utils/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const finnMiljoStreng = () => {
+    const host = window.location.host;
+    const bindestrekIndex = host.indexOf('-');
+    if (bindestrekIndex === -1) {
+        return '';
+    }
+    const dotIndex = host.indexOf('.');
+    return host.substring(bindestrekIndex, dotIndex);
+};
