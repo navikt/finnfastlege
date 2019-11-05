a19c4833966979f3451e06780f74aa4a64d224c9
Kristian Storvoll <kristianstorvoll@gmail.com>
2019-11-05 15:18:11 +0100

    test with axios instead of fetch

--- src/app/js/api/index.js
+++ src/app/js/api/index.js
@@ -1,75 +1,75 @@
 import { Error403 } from './errors';
+import axios from 'axios';
 
-const log = () => {
-};
+const log = () => {};
 
-export const getCookie = (name) => {
+export const getCookie = name => {
     const re = new RegExp(`${name}=([^;]+)`);
     const match = re.exec(document.cookie);
     return match !== null ? match[1] : '';
 };
 
 export function get(url) {
-    return fetch(url, {
-        credentials: 'include',
-    })
-        .then((res) => {
+    return axios
+        .get(url, {
+            withCredentials: true
+        })
+        .then(res => {
             if (res.status === 404) {
                 throw new Error('404');
             }
             if (res.status > 400 && res.status !== 403) {
                 throw new Error('Det oppstod en feil');
             }
-            return res.json().then((data) => {
+            return res.json().then(data => {
                 if (res.status === 403) {
                     const tilgang = {
                         harTilgang: false,
-                        begrunnelse: data.message,
+                        begrunnelse: data.message
                     };
                     throw new Error403('403', 403, tilgang);
                 }
                 return data;
             });
         })
-        .catch((err) => {
+        .catch(err => {
             log(err);
             throw err;
         });
 }
 
 export function post(url, body) {
-    return fetch(url, {
-        credentials: 'include',
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json',
-            NAV_CSRF_PROTECTION: getCookie('NAV_CSRF_PROTECTION'),
-        },
-    })
-    .then((res) => {
-        if (res.status >= 400) {
-            log(res);
-            throw new Error('Forespørsel feilet');
-        } else {
-            return res;
-        }
-    })
-    .catch((err) => {
-        log(err);
-        throw err;
-    });
+    return axios
+        .post(url, body, {
+            headers: {
+                'Content-Type': 'application/json',
+                NAV_CSRF_PROTECTION: getCookie('NAV_CSRF_PROTECTION')
+            },
+            withCredentials: true
+        })
+        .then(res => {
+            if (res.status >= 400) {
+                log(res);
+                throw new Error('Forespørsel feilet');
+            } else {
+                return res;
+            }
+        })
+        .catch(err => {
+            log(err);
+            throw err;
+        });
 }
 
 export function getWithoutThrows(url) {
     return fetch(url, {
-        credentials: 'include',
+        credentials: 'include'
     })
-    .then((res) => {
-        return res.json();
-    })
-    .catch((err) => {
-        log(err);
-        throw err;
-    });
+        .then(res => {
+            return res.json();
+        })
+        .catch(err => {
+            log(err);
+            throw err;
+        });
 }
