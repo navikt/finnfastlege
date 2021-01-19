456a2db878fe331a258b0bda283edc8809108a39
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-01-19 19:31:32 +0100

    Fix broken urls for diskresjonskode and egenansatt
    Endpoint to get Egenansatt and Diskresjonskode og person is moved from Modiasyforest to Syfoperson and the urls must therefore be updated.

--- src/app/js/sagas/egenansattSagas.js
+++ src/app/js/sagas/egenansattSagas.js
@@ -4,12 +4,12 @@ import { get } from '../api/index';
 import * as actions from '../actions/egenansatt_actions';
 import * as actiontyper from '../actions/actiontyper';
 
-const MODIASYFOREST_ROOT = '/modiasyforest/api/internad';
+const SYFOPERSON_ROOT = '/syfoperson/api';
 
 export function* hentEgenansattSaga(action) {
     yield put(actions.henterEgenansatt());
     try {
-        const data = yield call(get, `${MODIASYFOREST_ROOT}/egenansatt/${action.fnr}`);
+        const data = yield call(get, `${SYFOPERSON_ROOT}/person/egenansatt/${action.fnr}`);
         yield put(actions.egenansattHentet(data));
     } catch (e) {
         yield put(actions.hentEgenansattFeilet());
