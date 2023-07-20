3dbc4276da4b9bce0ca9e67565bf85a2f8ef20a8
Eirik Dahlen <dahleneirik@gmail.com>
2023-07-20 11:48:03 +0200

    Add frontend-faro-envs manually

--- src/faro/faro.ts
+++ src/faro/faro.ts
@@ -8,19 +8,19 @@ import nais from "../../nais.js";
 export const initFaro = (): Faro => {
   const telemetryCollectorUrl =
     process.env.NAIS_FRONTEND_TELEMETRY_COLLECTOR_URL;
-  const appName = process.env.NAIS_APP_NAME;
-  const appVersion = process.env.NAIS_APP_IMAGE;
+  // const appName = process.env.NAIS_APP_NAME;
+  // const appVersion = process.env.NAIS_APP_IMAGE;
   console.log("Telemetry URL: ", nais.telemetryCollectorURL);
   console.log("Telemetry URL: ", telemetryCollectorUrl);
   console.log("App ", nais.app.name);
-  console.log("App ", appName);
+  // console.log("App ", appName);
   console.log("Version ", nais.app.version);
-  console.log("Version ", appVersion);
+  // console.log("Version ", appVersion);
   return initializeFaro({
     url: telemetryCollectorUrl,
     app: {
-      name: appName,
-      version: appVersion,
+      name: "finnfastlege",
+      // version: appVersion,
     },
     instrumentations: [...getWebInstrumentations()],
   });
