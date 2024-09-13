d826743fe05bf22f7350efb3f6995b53dc3c81c3
geir-waagboe <geir.arne.waagbo@nav.no>
2024-09-13 13:57:22 +0200

    IS-2300: internarbeidsflate-decorator-v3

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,3 +1,4 @@
+export const MODIACONTEXTHOLDER_ROOT = "/modiacontextholder/api";
 export const SYFOPERSON_ROOT = "/syfoperson/api/v2";
 export const FASTLEGEREST_ROOT = "/fastlegerest/api/v2";
 export const TILGANGSKONTROLL_AD_PATH =
---
+++ src/data/modiacontext/useAktivBruker.ts
@@ -0,0 +1,14 @@
+import { post } from "@/api/axios";
+import { MODIACONTEXTHOLDER_ROOT } from "@/api/constants";
+import { useMutation } from "@tanstack/react-query";
+
+const NY_AKTIV_BRUKER = "NY_AKTIV_BRUKER";
+
+export const useAktivBruker = () =>
+  useMutation({
+    mutationFn: (fnr: string) =>
+      post(`${MODIACONTEXTHOLDER_ROOT}/context`, {
+        verdi: fnr,
+        eventType: NY_AKTIV_BRUKER,
+      }),
+  });
