4f88d5af2b4bf839a58f61233054039461cf987f
Peter Rydberg <peter.rydberg@bekk.no>
2026-07-01 12:38:27 +0200

    IS-4114: Fix view access to finnfastlege

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -2,4 +2,4 @@ export const MODIACONTEXTHOLDER_ROOT = "/modiacontextholder/api";
 export const SYFOPERSON_ROOT = "/syfoperson/api/v2";
 export const FASTLEGEREST_ROOT = "/fastlegerest/api/v2";
 export const TILGANGSKONTROLL_AD_PATH =
-  "/istilgangskontroll/api/tilgang/navident/syfo";
+  "/istilgangskontroll/api/tilgang/navident/finnfastlege";
